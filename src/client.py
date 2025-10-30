fd9129d497bb54f76169bc24dab92f13e3caf546
Eviltrapgod <blazer19092008@gmail.com>
2025-10-30 22:07:57 +0300

    Replace Auth module to Client, Remove Auth, smal typing fixes

--- src/client.py
+++ src/client.py
@@ -1,17 +1,38 @@
-from httpx import AsyncClient
+from httpx import AsyncClient, Response
+from typing import cast 
+
 
 from .transport import Transport
-from .auth import Auth
-
+from .data import config 
+from .utils.app_key import ApplicationKey
 
 class Client:
     def __init__(self, client: AsyncClient) -> None:
-        self._client: AsyncClient = client
-        self._auth: Auth = Auth(self._client)
-        self._transport: Transport = Transport(self._client)
+        self._client    : AsyncClient = client
+        self._transport : Transport = Transport(self._client)
+        self._app_key   : ApplicationKey = ApplicationKey()
 
-    async def login(self, username: str, password: str) -> str:
-        return await self._auth.get_jwt_token(username, password)
+    async def login(self, username: str, password: str):
+        _auth_data = {
+            "application_key": await self._app_key.get_key(True),
+            "username": username,
+            "password": password,
+            "id_city": None,
+        }
+
+        _response: Response = await self._transport.request(
+            method="post",
+            url=config.AUTH_URL,
+            json=_auth_data,
+            timeout=2.0,
+        )
+
+        _jwt_token: str = str(
+            cast(dict[str, str], _response.json()).get("access_token", "")
+        )
+
+        if _jwt_token:
+            return _jwt_token
 
     async def get_schedule(self, token: str, date: str):
         pass
@@ -20,6 +41,5 @@ class Client:
         pass
 
     async def close_connection(self):
-        if self._client:
-            await self._client.aclose()
-            return
+        await self._client.aclose()
+        return
