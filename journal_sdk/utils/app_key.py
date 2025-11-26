783ed2d191be01c741b83644df1294d275e9c1d1
anatoliy <cool.afkshnik@gmail.com>
2025-11-26 22:00:26 +0300

    Оптимизирована и уменьшена кодовая база. Добавлены докстринги

--- journal_sdk/utils/app_key.py
+++ journal_sdk/utils/app_key.py
@@ -5,13 +5,28 @@ from bs4 import BeautifulSoup
 
 
 class ApplicationKey:
-    def __init__(self):
-        self.__base_url: str = "https://journal.top-academy.ru"
-        self.__app_js_url: str = ""
-        self.__app_token: str | None = ""
+    """Класс для получения и хранения ключа приложения из JavaScript-файлов сайта."""
 
-    def __parse_index_html(self, html: str) -> str:
-        soup = BeautifulSoup(html, "html.parser")
+    def __init__(self, journal_base_url: str) -> None:
+        """Инициализирует экземпляр класса с базовым URL и пустым значением ключа приложения.
+
+        Args:
+            journal_base_url: Базовый URL журнала для поиска JavaScript-файлов.
+        """
+        self.journal_base_url: str = journal_base_url
+        self.__app_key: str = ""
+
+    def __get_js_url(self, root_html: str) -> str:
+        """
+        Парсит HTML-страницу и находит ссылку на JavaScript-файл приложения.
+
+        Args:
+            root_html: HTML-содержимое страницы.
+
+        Returns:
+            Полный URL JavaScript-файла приложения или пустую строку, если не найден.
+        """
+        soup = BeautifulSoup(root_html, "html.parser")
         scripts = soup.find_all("script")
         target_script: str = ""
         for script in scripts:
@@ -19,32 +34,43 @@ class ApplicationKey:
             if src and "app." in src and src.endswith(".js"):
                 target_script = src
                 break
-        return target_script
+        return self.journal_base_url + target_script
 
-    async def __get_app_js_url(self, refresh: bool = False) -> str:
-        if self.__app_js_url == "" or refresh:
-            async with httpx.AsyncClient() as client:
-                resp = await client.get(self.__base_url)
-                script_url = self.__parse_index_html(html=resp.text)
-                app_js_url = self.__base_url + script_url
-                self.__app_js_url = app_js_url
-                return app_js_url
-        else:
-            return self.__app_js_url
+    async def __get_app_key(self, js_text: str) -> str:
+        """
+        Извлекает ключ приложения из JavaScript-кода с помощью регулярного выражения.
 
-    async def __parse_app_js(self, js_text: str) -> str | None:
+        Args:
+            js_text: Текст JavaScript-файла.
+
+        Returns:
+            Извлеченный ключ приложения или пустую строку, если ключ не найден.
+        """
         pattern = r'o\.authModel\s*=\s*new\s*r\.AuthModel\("([^"]+)"\)'
         match = re.search(pattern, js_text)
         if match:
             token_value = match.group(1)
             return token_value
-
-    async def get_key(self, refresh: bool = False) -> str | None:
-        if self.__app_token == "" or refresh is True:
-            async with httpx.AsyncClient() as client:
-                resp = await client.get(await self.__get_app_js_url(refresh=True))
-                app_token = await self.__parse_app_js(resp.text)
-                self.__app_token = app_token
-                return app_token
         else:
-            return self.__app_token
+            return ""
+
+    async def get_key(self, refresh: bool = False) -> str:
+        """
+        Получает ключ приложения, при необходимости обновляя его.
+
+        Args:
+            refresh: Флаг, указывающий на необходимость принудительного обновления ключа.
+
+        Returns:
+            Ключ приложения или пустую строку, если ключ не найден.
+        """
+        if self.__app_key == "" or refresh is True:
+            async with httpx.AsyncClient() as client:
+                root_html_resp = await client.get(self.journal_base_url)
+                js_url = self.__get_js_url(root_html_resp.text)
+                js_resp = await client.get(js_url)
+                app_key = await self.__get_app_key(js_resp.text)
+                self.__app_key = app_key
+                return app_key
+        else:
+            return self.__app_key
